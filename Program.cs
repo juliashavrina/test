e55c7b20edd67a1143251c28c5e4f29377e6c4c6
«juliashavrina» <tifanys@yandex.ru>
2022-07-01 19:39:09 +0300

    Bool file

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿bool Second_task_1(int numberTask2)
+{
+  Console.WriteLine("Input:");
+  if (numberTask2%7==0 && numberTask2%23==0) 
+      return true;
+  else
+      return false;
+
+}
+int task2 = Convert.ToInt32(Console.ReadLine());
+Second_task_1(task2);
